bdaf5d1db56030d2ec3842ba7f9070bd24defaa9
djamalB <djamal.balaev@bk.ru>
2023-09-11 18:02:30 +0300

    os import

---
+++ index.js
@@ -0,0 +1,9 @@
+import * as os from  "os";
+import {name} from './welcome.js'
+console.log(os.homedir)
+console.log(os.hostname);
+console.log(os.version());
+console.log(os.type());
+
+
+console.log(name('djamal'))
---
+++ welcome.js
@@ -0,0 +1,5 @@
+import * as os from "os";
+export function name(name) {
+ return `Привет,${name}! Вижу ты зашел с ${os.type()}`
+}
+ 
